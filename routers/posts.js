f6cc07a6a7691fa4eeb035aaa2e91d65f8245449
nabil-hajali <nabil2491@gmail.com>
2026-04-07 11:39:08 +0200

    refactor: update dev script and move posts data to posts router

--- routers/posts.js
+++ routers/posts.js
@@ -2,6 +2,45 @@ const express = require('express');
 const router = express.Router();
 
 
+const posts = [
+  {
+    id: 1,
+    title: 'Benvenuto al mio blog',
+    content: 'Questo è il primo post del mio blog. Qui posso scrivere quello che voglio.',
+    image: '/images/post1.svg',
+    tags: ['welcome', 'intro']
+  },
+  {
+    id: 2,
+    title: 'Secondo post',
+    content: 'Un altro testo d esempio per il secondo post.',
+    image: '/images/post2.svg',
+    tags: ['news']
+  },
+  {
+    id: 3,
+    title: 'JavaScript Tips',
+    content: 'Consigli utili su JavaScript e Node.js.',
+    image: '/images/post3.svg',
+    tags: ['javascript','node']
+  },
+  {
+    id: 4,
+    title: 'Routing in Express',
+    content: 'Come gestire le rotte e gli asset statici con Express.',
+    image: '/images/post4.svg',
+    tags: ['express','routing']
+  },
+  {
+    id: 5,
+    title: 'Ultime novità',
+    content: 'Aggiornamenti e novità sul blog.',
+    image: '/images/post5.svg',
+    tags: ['update','news']
+  }
+];
+
+
 // server entry point
 router.get('/', (req, res) => {
   res.send('Server del mio blog');
